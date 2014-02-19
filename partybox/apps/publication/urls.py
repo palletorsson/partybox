184383c27382dd54ab9d5d7e12ddb93fe084c462
palle torsson <palle.torsson@gmail.com>
2014-02-19 23:36:16 +0100

    no traling

--- partybox/apps/publication/urls.py
+++ partybox/apps/publication/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, patterns
 from apps.publication.views import Posts, AddPost
 
 urlpatterns = patterns('',
-    url(r'^share/$', Posts, name= 'list_posts'),
+    url(r'^/*$', Posts, name= 'list_posts'),
     url(r'^add/$', AddPost, name= 'add_post'),
 )
 
