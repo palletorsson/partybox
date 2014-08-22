a08d8703d0a20b8b706e40a29b9aafc350aa97ad
plix <palle.torsson@gmail.com>
2014-08-22 12:25:44 +0000

    old settings

--- partybox/partybox/settings.py
+++ partybox/partybox/settings.py
@@ -93,7 +93,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/home/palle/raspberry/partybox_env/partybox/partybox/theme/templates/',
+    '/home/pi/partybox/partybox/partybox/theme/templates/',
 )
 
 try:
