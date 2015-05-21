922f694561e53590c898d847b5599cba46fc16d2
plix <palle.torsson@gmail.com>
2015-05-21 22:13:06 +0200

    Update settings.py

--- partybox/partybox/settings.py
+++ partybox/partybox/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'partybox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'partybox',
+        'USER': 'root',
+        'PASSWORD': 'raspberry',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
